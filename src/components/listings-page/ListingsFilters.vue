bdd7bf166d9ca452de60464416838d3b1ad41715
Catgirl <zeroaninea@gmail.com>
2026-05-08 16:52:30 +0400

    a type selection added

--- src/components/listings-page/ListingsFilters.vue
+++ src/components/listings-page/ListingsFilters.vue
@@ -1,4 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const location = ref('')
+const type = ref('')
+
+const search = () => {
+  setTimeout(() => {
+    router.push({
+      path: '/listings',
+      query: {
+        location: location.value,
+        type: type.value,
+      },
+    })
+  }, 300)
+}
+</script>
 
 <template>
   <div class="listings-filters">
@@ -6,17 +26,21 @@
       <div class="row">
         <div class="col-md-6">
           <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search Properties" />
-            <div class="input-group-append">
-              <button
-                class="btn btn-primary"
-                v-ripple="
-                  'color-mix(in oklab, var(--bs-blue) 75%, var(--bs-white) 15%, transparent)'
-                "
-              >
-                Search
-              </button>
-            </div>
+            <input v-model="location" class="form-control" placeholder="Location" />
+
+            <select v-model="type" class="form-select">
+              <option value="">Any Type</option>
+              <option value="apartment">Apartment</option>
+              <option value="house">House</option>
+            </select>
+
+            <button
+              @click="search"
+              class="btn btn-primary"
+              v-ripple="'color-mix(in oklab, var(--bs-blue) 75%, var(--bs-white) 15%, transparent)'"
+            >
+              <span class="btn-content">Search</span>
+            </button>
           </div>
         </div>
       </div>
