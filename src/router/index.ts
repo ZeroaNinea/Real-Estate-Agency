16b1a86abb46b4b4697dc6f7ae15f6fa1499dd6f
Catgirl <zeroaninea@gmail.com>
2026-05-08 12:02:48 +0400

    a new /listings view added

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,11 @@ const router = createRouter({
       name: 'home',
       component: () => import('@/views/HomeView.vue'),
     },
+    {
+      path: '/listings',
+      name: 'listings',
+      component: () => import('@/views/ListingsView.vue'),
+    },
   ],
 })
 
