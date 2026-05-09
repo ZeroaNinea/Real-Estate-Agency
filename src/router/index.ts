c37829bf4771e3c05190c348ad979c56c499d72f
Catgirl <zeroaninea@gmail.com>
2026-05-10 02:12:51 +0400

    use only one route

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,11 +9,11 @@ const router = createRouter({
       name: 'home',
       component: () => import('@/views/HomeView.vue'),
     },
-    {
-      path: '/listings',
-      name: 'listings',
-      component: () => import('@/views/ListingsView.vue'),
-    },
+    // {
+    //   path: '/listings',
+    //   name: 'listings',
+    //   component: () => import('@/views/ListingsView.vue'),
+    // },
   ],
 })
 
