b20e42c4b96ad7a81618ba5da167543c59f56598
Catgirl <zeroaninea@gmail.com>
2026-05-09 17:10:09 +0400

    pagination buttons added

---
+++ src/components/listings-page/ListingsPagination.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{ pages: number }>()
+</script>
+
+<template>
+  <div class="container">
+    <ul class="pagination justify-content-center">
+      <li class="page-item mx-1" v-for="page in pages" :key="page">
+        <button
+          v-ripple="'color-mix(in lch, var(--bs-black) 45%, transparent)'"
+          class="page-link btn"
+        >
+          <span class="btn-content">{{ page }}</span>
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+/* Button fix. */
+button {
+  isolation: isolate;
+}
+
+.btn-content {
+  position: relative;
+  z-index: 1;
+}
+</style>
--- src/views/ListingsView.vue
+++ src/views/ListingsView.vue
@@ -4,6 +4,7 @@ import { computed, ref } from 'vue'
 import ListingsTitle from '@/components/listings-page/ListingsTitle.vue'
 import ListingsFilters from '@/components/listings-page/ListingsFilters.vue'
 import ListingsGrid from '@/components/listings-page/ListingsGrid.vue'
+import ListingsPagination from '@/components/listings-page/ListingsPagination.vue'
 
 import { properties } from '@/mock/properties'
 
@@ -20,6 +21,7 @@ const paginatedProperties = computed(() => {
   <ListingsTitle />
   <ListingsFilters />
   <ListingsGrid :properties="paginatedProperties" />
+  <ListingsPagination :pages="Math.ceil(properties.length / itemsPerPage)" />
 </template>
 
 <style scoped></style>
