f96b2e81346c2a2d36dff607637c5a9e7bb609e2
Catgirl <zeroaninea@gmail.com>
2026-05-09 16:50:31 +0400

    a basic pagination added

--- src/views/ListingsView.vue
+++ src/views/ListingsView.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue'
+
 import ListingsTitle from '@/components/listings-page/ListingsTitle.vue'
 import ListingsFilters from '@/components/listings-page/ListingsFilters.vue'
 import ListingsGrid from '@/components/listings-page/ListingsGrid.vue'
 
 import { properties } from '@/mock/properties'
+
+const currentPage = ref(1)
+const itemsPerPage = 6
+
+const paginatedProperties = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage
+  return properties.slice(start, start + itemsPerPage)
+})
 </script>
 
 <template>
   <ListingsTitle />
   <ListingsFilters />
-  <ListingsGrid :properties="properties" />
+  <ListingsGrid :properties="paginatedProperties" />
 </template>
 
 <style scoped></style>
