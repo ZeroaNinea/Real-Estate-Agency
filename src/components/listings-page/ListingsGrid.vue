066297fb63d452840fd0bd6da20a967e18cd12f4
Catgirl <zeroaninea@gmail.com>
2026-05-09 15:43:18 +0400

    ListingsGrid added

---
+++ src/components/listings-page/ListingsGrid.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import PropertyCard from '@/components/shared/PropertyCard.vue'
+import { properties } from '@/mock/properties'
+</script>
+
+<template>
+  <section class="container py-5">
+    <h1>Listings</h1>
+    <p>Listing cards go here</p>
+
+    <div v-if="properties.length > 0" class="row g-4">
+      <div class="col-md-6 col-lg-4" v-for="property in properties" :key="property.id">
+        <PropertyCard :property="property" />
+      </div>
+    </div>
+    <div v-else>
+      <p>X_X No properties found...</p>
+    </div>
+  </section>
+</template>
--- src/views/ListingsView.vue
+++ src/views/ListingsView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import ListingsTitle from '@/components/listings-page/ListingsTitle.vue'
 import ListingsFilters from '@/components/listings-page/ListingsFilters.vue'
+import ListingsGrid from '@/components/listings-page/ListingsGrid.vue'
 </script>
 
 <template>
   <ListingsTitle />
   <ListingsFilters />
+  <ListingsGrid />
 </template>
 
 <style scoped></style>
