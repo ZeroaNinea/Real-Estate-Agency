e44fdd44a216a45284f483b768ae1ca01982de78
Catgirl <zeroaninea@gmail.com>
2026-05-09 21:56:22 +0400

    trying to deploy again

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  // base: './',
   // Add only for deployment with `gh-pages`.
-  // base: '/Real-Estate-Agency/',
+  base: './Real-Estate-Agency/',
 })
