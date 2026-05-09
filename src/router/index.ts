27cd29420a2e2785baace8dc26b40e1ae474b827
Catgirl <zeroaninea@gmail.com>
2026-05-10 01:23:25 +0400

    dev mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 // `createWebHistory` replaced with `createWebHashHistory` for production.
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
