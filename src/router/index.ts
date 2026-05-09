d01666693a8299f82bfb3095afc0b8e24be4a44d
Catgirl <zeroaninea@gmail.com>
2026-05-10 01:08:43 +0400

    replace createWebHistory with createWebHashHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// `createWebHistory` replaced with `createWebHashHistory` for production.
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
