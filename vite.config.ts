194941987eaeed9f58d031a5749884913d7a5b54
Catgirl <zeroaninea@gmail.com>
2026-05-10 12:38:34 +0400

    deploying again

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   // Remove the `vueDevTools` plugin for production.
-  plugins: [vue(), vueDevTools()],
-  // plugins: [vue()],
+  // plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   // Add only for deployment with `gh-pages`.
-  // base: '/Real-Estate-Agency/',
+  base: '/Real-Estate-Agency/',
 })
