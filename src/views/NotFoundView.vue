0cce6d19a7f7b80551db73a5ac313935f856cbd3
Catgirl <zeroaninea@gmail.com>
2026-05-10 19:02:57 +0400

    center the text

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container py-5">
     <div class="row">
-      <div class="col-12">
-        <h1>404</h1>
-        <h2>Page Not Found</h2>
+      <div class="col-12 text-center">
+        <h1 class="display-3">404</h1>
+        <h2 class="display-5">Page Not Found</h2>
       </div>
     </div>
   </div>
