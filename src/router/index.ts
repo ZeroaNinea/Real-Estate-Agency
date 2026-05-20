ec402ddb7cbd984cc0dc18b0799fa86982ce23ab
Catgirl <zeroaninea@gmail.com>
2026-05-20 21:52:13 +0400

    about view added

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,6 +20,11 @@ const router = createRouter({
       component: () => import('@/views/PropertyDetailsView.vue'),
     },
     {
+      path: '/about',
+      name: 'about',
+      component: () => import('@/views/AboutView.vue'),
+    },
+    {
       path: '/:pathMatch(.*)*',
       name: 'not-found',
       component: () => import('@/views/NotFoundView.vue'),
---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container py-5">
+    <!-- <AboutCompanySection /> -->
+    <!-- <TeamMembersSection /> -->
+  </div>
+</template>
