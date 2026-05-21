fa92a47b25032645e7dd12ab6d4fe5fa32c375f3
Catgirl <zeroaninea@gmail.com>
2026-05-21 11:54:24 +0400

    the contact route added

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,6 +25,11 @@ const router = createRouter({
       component: () => import('@/views/AboutView.vue'),
     },
     {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('@/views/ContactView.vue'),
+    },
+    {
       path: '/:pathMatch(.*)*',
       name: 'not-found',
       component: () => import('@/views/NotFoundView.vue'),
