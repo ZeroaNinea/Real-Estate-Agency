0f4631e0abe349f1f77756af033d54d47c6231b6
Catgirl <zeroaninea@gmail.com>
2026-05-24 14:12:53 +0400

    dark mode added

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,24 @@
 import { RouterView } from 'vue-router'
 import AppHeader from '@/components/layout/AppHeader.vue'
 import CallToAction from '@/components/layout/CallToAction.vue'
+
+import { onMounted, onUnmounted } from 'vue'
+
+const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+const updateTheme = (event?: MediaQueryList | MediaQueryListEvent) => {
+  document.body.setAttribute('data-bs-theme', event?.matches ? 'dark' : 'light')
+}
+
+onMounted(() => {
+  updateTheme(mediaQuery)
+
+  mediaQuery.addEventListener('change', updateTheme)
+})
+
+onUnmounted(() => {
+  mediaQuery.removeEventListener('change', updateTheme)
+})
 </script>
 
 <template>
